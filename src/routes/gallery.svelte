e000553eb15edfaf4b279b4bc68f5c9ba67afd4c
francesco-cori <francesco.corigliano@nexev.it>
2022-01-12 12:29:15 +0100

    aggiunti dipinti a sezione gallery

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -46,4 +46,161 @@
 		</div>
 	</div>
 	<br />
+
+	<section>
+		<div
+			class="
+    relative
+    items-center
+    w-full
+    px-5
+    py-12
+    mx-auto
+    md:px-12
+    lg:px-24
+    max-w-7xl
+  "
+		>
+			<div class="grid w-full grid-cols-1 gap-6 mx-auto md:grid-cols-2 lg:grid-cols-3">
+				<div class="p-6">
+					<img
+						class="hover:-translate-y-1 hover:scale-110 duration-300
+          object-cover object-center
+          w-full
+          mb-8
+          lg:h-48
+          md:h-36
+          rounded-xl
+        "
+						src="https://www.storicang.it/medio/2021/07/22/van-gogh-dipinse-la-straordinaria-notte-stellata-nel-1889_57fea25a_800x620.jpg"
+						alt="blog"
+					/>
+					<h1
+						class="
+          mx-auto
+          mb-8
+          text-2xl
+          font-semibold
+          leading-none
+          tracking-tighter
+          text-neutral-600
+          lg:text-3xl
+        "
+					>
+						Titolo
+					</h1>
+					<p class="mx-auto text-base leading-relaxed text-gray-500">Anno</p>
+					<div class="mt-4">
+						<a
+							href="#"
+							class="
+            inline-flex
+            items-center
+            mt-4
+            font-semibold
+            text-blue-600
+            lg:mb-0
+            hover:text-neutral-600
+          "
+							title="read more"
+						>
+							Read More »
+						</a>
+					</div>
+				</div>
+				<div class="p-6">
+					<img
+						class="hover:-translate-y-1 hover:scale-110 duration-300 
+          object-cover object-center
+          w-full
+          mb-8
+          lg:h-48
+          md:h-36
+          rounded-xl
+        "
+						src="https://arte.sky.it/archivio/wp-content/uploads/2015/08/Van-Gogh-HR-620x388.jpg"
+						alt="blog"
+					/>
+					<h1
+						class="
+          mx-auto
+          mb-8
+          text-2xl
+          font-semibold
+          leading-none
+          tracking-tighter
+          text-neutral-600
+          lg:text-3xl
+        "
+					>
+						Titolo
+					</h1>
+					<p class="mx-auto text-base leading-relaxed text-gray-500">Anno</p>
+					<div class="mt-4">
+						<a
+							href="#"
+							class="
+            inline-flex
+            items-center
+            mt-4
+            font-semibold
+            text-blue-600
+            lg:mb-0
+            hover:text-neutral-600
+          "
+							title="read more"
+						>
+							Read More »
+						</a>
+					</div>
+				</div>
+				<div class="p-6">
+					<img
+						class="hover:-translate-y-1 hover:scale-110 duration-300
+          object-cover object-center
+          w-full
+          mb-8
+          lg:h-48
+          md:h-36
+          rounded-xl
+        "
+						src="https://i2.wp.com/orizzontecultura.com/wp-content/uploads/2020/10/VAN_GOGH_1470..jpg?resize=930%2C620&ssl=1"
+						alt="blog"
+					/>
+					<h1
+						class="
+          mx-auto
+          mb-8
+          text-2xl
+          font-semibold
+          leading-none
+          tracking-tighter
+          text-neutral-600
+          lg:text-3xl
+        "
+					>
+						Titolo
+					</h1>
+					<p class="mx-auto text-base leading-relaxed text-gray-500">Anno</p>
+					<div class="mt-4">
+						<a
+							href="#"
+							class="
+            inline-flex
+            items-center
+            mt-4
+            font-semibold
+            text-blue-600
+            lg:mb-0
+            hover:text-neutral-600
+          "
+							title="read more"
+						>
+							Read More »
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </section>
